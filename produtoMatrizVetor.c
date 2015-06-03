e501fdcfcf1e3ea6a92fb65b18bf190c71b8ef98
Felipe Faria <felipesfaria@gmail.com>
2015-06-03 11:24:19 -0300

    Clean some warnings

--- produtoMatrizVetor.c
+++ produtoMatrizVetor.c
@@ -2,9 +2,9 @@
 /* Introdução ao MPI (biblioteca de troca de mensagens) */
 /* Mostra como usar as funções de distribuição e coleta de dados distribuídos */
 
-/* Compile:  mpicc -o produtoMatrizVetor produtoMatrizVetor.c */
+/* Compile:  mpicc -O -o produtoMatrizVetor produtoMatrizVetor.c */
 /* Run:      mpirun -np <number of processes> ./produtoMatrizVetor <tamanho do vetor> <debug_flag>*/
-/* Exm:      mpirun -np 2 ./produtoMatrizVetor 5 0*/
+/* Exm:      mpirun -np 2 ./produtoMatrizVetor 1000*/
 /* --------------------------------------------------------------------------------------------*/
 
 #include "mpi.h"
@@ -17,7 +17,7 @@ typedef int bool;
 #define false 0
 
 #define SEED 1
-//#define DEBUG 1
+#define DEBUG 1
 
 int** InitMatriz(int n, bool cheia);
 int** InitMatrizCheia(int tamanhoMatriz);
@@ -48,15 +48,14 @@ bool ComparaVetores(int* A, int* B, int tamanho){
 }
 
 int main(int argc, char *argv[]) {
-   int rank, nprocs, i, j, ret, local_flag=0, total_flag=0,DEBUG;
-   int *a=NULL, *local_a=NULL, *b=NULL, size, local_n;
-   int *A,*X,*Y,*R;
+   int rank, nprocs, i, j, ret, local_flag=0, total_flag=0;
+   int *local_a=NULL, size, local_n;
+   int *A=NULL,*X=NULL,*Y=NULL,*R=NULL;
    double startTimeParallel,endTimeParallel, elapsedTimeParallel;
    double startTimeSequential,endTimeSequential, elapsedTimeSequential;
 
    if(SEED) srand(SEED);
    else srand(time(NULL));
-
 
    //inicializa o MPI
    ret = MPI_Init(&argc, &argv);
@@ -66,36 +65,42 @@ int main(int argc, char *argv[]) {
    } else return 0;
 
    //recebe o numero de elementos do vetor
-   if(argc<3) {
-      if(rank==0) printf("Digite: %s <numero de elementos do vetor> <debug_flag>\n", argv[0]);
+   if(argc<2) {
+      if(rank==0) printf("Digite: %s <numero de elementos do vetor>\n", argv[0]);
       MPI_Finalize();
       return 0;
    }
    size = atoi(argv[1]);
-   DEBUG = atoi(argv[2]);
 
     X = malloc( size*sizeof( int ));
+    if(X==NULL) local_flag = 1; //sinaliza que houve erro
    //aloca espaco para o vetor de entrada e de saida
    if (rank == 0) {
        A = malloc( size*size*sizeof( int ));
-       for(i=0;i<size*size;i++) A[i]=rand() % 10;
+#ifdef DEBUG
+        printf("A\n");
+#endif
        if(A==NULL) local_flag = 1; //sinaliza que houve erro
+       for(i=0;i<size*size;i++) A[i]=i;
+
        Y = malloc( size*sizeof( int ));
        if(Y==NULL) local_flag = 1; //sinaliza que houve erro
+#ifdef DEBUG
+        printf("Y:\n");
+#endif
 
-       if(X==NULL) local_flag = 1; //sinaliza que houve erro
        for(i=0;i<size;i++) X[i]=rand() % 10;
    }
+#ifdef DEBUG
     if(rank==0){
-        if(DEBUG){
-            printf("A:\n");
-            PrintMatriz(A,size);
-            printf("X:\n");
-            PrintVetor(X,size);
-        }
+        printf("A:\n");
+        PrintMatriz(A,size);
+        printf("X:\n");
+        PrintVetor(X,size);
     }
+#endif
    //aloca espaco para o bloco do vetor em cada processo
-   local_n = size/nprocs*size;
+   local_n = (size/nprocs)*size;
    local_a = (int*) malloc(local_n * sizeof(int));
    if (local_a == NULL) local_flag = 1; //sinaliza que houve falha
 
@@ -107,35 +112,49 @@ int main(int argc, char *argv[]) {
       MPI_Finalize(); //encerra todos os processos
       return 0;
    }
-    if(rank==0&&!DEBUG) GET_TIME(startTimeParallel);
+    #ifndef DEBUG
+        if(rank==0) GET_TIME(startTimeParallel);
+    #endif
    //divide o vetor em blocos eentrega um bloco por processo
    MPI_Scatter(A, local_n, MPI_INT, local_a, local_n, MPI_INT, 0, MPI_COMM_WORLD);
    int minhasLinhas = size/nprocs;
    int* acumulador = malloc(minhasLinhas*sizeof(int));
    //faz o processamento em cada processo
    int offset;
-   if(DEBUG) printf("minhasLinhas=%d\n",minhasLinhas);
-   if(DEBUG) printf("[%d]:for(i =%d;i<%d;i++)\n",rank,0,minhasLinhas);
+#ifdef DEBUG
+printf("minhasLinhas=%d\n",minhasLinhas);
+printf("[%d]:for(i =%d;i<%d;i++)\n",rank,0,minhasLinhas);
+#endif
    for(i =0;i<minhasLinhas;i++){
        acumulador[i]=0;
        for (j = 0; j < size; j++){
           offset = j+i*size;
-          if(DEBUG) printf("[%d]:acumulador[%d]+=local_a[%d]*X[%d];\n",rank,i,offset,j);
+#ifdef DEBUG
+printf("[%d]:acumulador[%d]+=local_a[%d]*X[%d];\n",rank,i,offset,j);
+#endif
           acumulador[i]+=local_a[offset]*X[j];
        }
-       if(DEBUG) printf("p:[%d] acumulador[%d]==%d\n",rank,i, acumulador[i]);
+       #ifdef DEBUG
+       printf("p:[%d] acumulador[%d]==%d\n",rank,i, acumulador[i]);
+       #endif
    }
    if(rank==0){
-   if(DEBUG) printf("%d*%d!=%d\n",minhasLinhas,nprocs,size);
+#ifdef DEBUG
+printf("%d*%d!=%d\n",minhasLinhas,nprocs,size);
+#endif
     if(minhasLinhas*nprocs!=size){
         for(i=minhasLinhas*nprocs;i<size;i++){
            Y[i]=0;
            for (j = 0; j < size; j++){
               offset = j+i*size;
-              if(DEBUG) printf("[%d]:Y[%d]+=A[%d]*X[%d];\n",rank,i,offset,j);
+#ifdef DEBUG
+printf("[%d]:Y[%d]+=A[%d]*X[%d];\n",rank,i,offset,j);
+#endif
               Y[i]+=A[offset]*X[j];
            }
-           if(DEBUG) printf("p:[%d] Y[%d]==%d\n",rank,i, Y[i]);
+#ifdef DEBUG
+if(DEBUG) printf("p:[%d] Y[%d]==%d\n",rank,i, Y[i]);
+#endif // DEBUG
         }
     }
    }
@@ -144,24 +163,26 @@ int main(int argc, char *argv[]) {
    //coleta os blocos processados por cada processo
    MPI_Gather(acumulador, minhasLinhas, MPI_INT, Y, minhasLinhas, MPI_INT, 0, MPI_COMM_WORLD);
 
-    if(rank==0&&!DEBUG) GET_TIME(endTimeParallel);
+    #ifndef DEBUG
+        if(rank==0) GET_TIME(endTimeParallel);
+    #endif
    //imprime o vetor resultante
    if (rank == 0) {
+    #ifndef DEBUG
       GET_TIME(startTimeSequential);
       R=ProdutoSequencial(A,X,size);
       GET_TIME(endTimeSequential);
-      elapsedTimeSequential = endTimeSequential - startTimeSequential;
-    if(!DEBUG){
+        elapsedTimeSequential = endTimeSequential - startTimeSequential;
         elapsedTimeParallel = endTimeParallel-startTimeParallel;
         printf("Sequential Time:%.6f\n",elapsedTimeSequential);
         printf("Parallel Time:%.6f\n",elapsedTimeParallel);
         printf("Speedup:%.6f\n",elapsedTimeSequential/elapsedTimeParallel);
-    }
-    if(DEBUG){
+    #endif // DEBUG
+    #ifdef DEBUG
         printf("Y:\n");
         PrintVetor(Y,size);
         PrintVetor(R,size);
-    }
+    #endif
       if(ComparaVetores(Y,R,size)) printf("Correto\n");
       else printf("Errado\n");
 
@@ -201,35 +222,5 @@ void PrintVetor(int* vetor, int tamanhoVetor){
         printf("%d ",vetor[j]);
     }
     printf("\n");
-}
-
-int** InitMatrizCheia(int tamanhoMatriz){
-    return InitMatriz(tamanhoMatriz,true);
-}
-
-int** InitMatrizVazia(int tamanhoMatriz){
-    return InitMatriz(tamanhoMatriz,false);
-}
-
-int** InitMatriz(int n,bool cheia){
-    int i,j;
-    int** matriz;
-    if (( matriz = malloc( n*sizeof( int* ))) == NULL )
-    { /* error */ }
-    for ( i = 0; i < n; i++ )
-    {
-        if (( matriz[i] = malloc( n*sizeof( int* ) )) == NULL )
-        { /* error */ }
-        if(cheia){
-            for(j=0;j<n;j++){
-                matriz[i][j]=(j+j+i);
-            }
-        }else{
-            for(j=0;j<n;j++){
-                matriz[i][j]=0;
-            }
-        }
-    }
-    return matriz;
 }
 
